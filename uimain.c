d9806ffc3bc72c09402943ae097881fa3e4aa1d0
Nocturnevh <vaherrera6@miners.utep.edu>
2025-06-15 23:50:50 -0600

    Feature: Added Arrow and Font 11x16 printing feature e.

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, Chars = c, My Char = m) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -28,6 +28,15 @@ int main()
       puts("You selected chars:");
       for (char c = 'a'; c < 'd'; c++)
 	print_char_5x7(c);
+      break;
+    case 'm':
+      puts("You selected my chars:");
+      for(char c = 'a'; c < 'd'; c++)
+	print_char_11x16(c);
+      break;
+    case 'a': /* Arrow  */
+      puts("You selected arrow:");
+      print_arrow();
       break;
     case 'q':
       puts("Bye!");
