e2494bd77ce58591c4af7ebba7665d821720a82d
Nocturnevh <vaherrera6@miners.utep.edu>
2025-06-15 23:49:42 -0600

    Feature: Added function to print font 11x16.

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,19 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+
+/* Ptints out font 11x16. */
+void print_char_11x16(char c)
+{
+  c -= 0x20;
+  for(char col = 0; col < 11; col++){
+    for(char row = 0; row < 16; row++){
+      unsigned short rowBits = font_11x16[c][col];
+      unsigned short colMask = 1 << (15-row);
+      putchar((rowBits & colMask) ? '*': ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
